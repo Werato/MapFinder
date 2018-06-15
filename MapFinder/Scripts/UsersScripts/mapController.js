306df170eb7476ac245a8413443dd5f87f0fe93f
DESKTOP-MRS7EUR\Sevar <emil.nersesjan@btf.lv>
2018-06-15 16:31:23 +0300

    Just err point coord

--- MapFinder/Scripts/UsersScripts/mapController.js
+++ MapFinder/Scripts/UsersScripts/mapController.js
@@ -28,7 +28,7 @@ $(function () {
         var feature = new ol.Feature({
             type: 'point',
             //geometry: new ol.geom.Point(ol.proj.fromLonLat(coord))
-            geometry: new ol.geom.Point(ol.proj.transform(coord, 'EPSG: 4326', 'EPSG: 3857'))//{ 0:coord[i].Lon, 1: }ol.proj.fromLonLat(coord)//[coord[0], coord[1]]
+            geometry: new ol.geom.Point(ol.proj.transform(to4326(coord)))//{ 0:coord[i].Lon, 1: }ol.proj.fromLonLat(coord)//[coord[0], coord[1]]
         });
         feature.setStyle(styles);
         vectorSource.addFeature(feature);
@@ -65,16 +65,6 @@ $(function () {
    
     });
 
-    //TEST DRAW
-    //var thing = new ol.geom.Polygon([[
-    //    ol.proj.transform([-16, -22], 'EPSG:4326', 'EPSG:3857'),
-    //    ol.proj.transform([-44, -55], 'EPSG:4326', 'EPSG:3857')
-    //]]);
-    //var featurething = new ol.Feature({
-    //    name: "Thing",
-    //    geometry: thing
-    //});
-    //vectorSource.addFeature(featurething);
     /*
     stylePointFunction = function (custom_img) {
         //var src = "/_dgv/proxy.img.php?paper_blueprint:" + custom_img;
@@ -182,6 +172,19 @@ $(function () {
             }
         })
     }
+
+    function addInteractions() {
+        draw = new ol.interaction.Draw({
+            source: vectorSource,
+            style: styles,
+            type: 'Point'
+        });
+        map.addInteraction(draw);
+        snap = new ol.interaction.Snap({ source: source });
+        map.addInteraction(snap);
+    }
+
+
     function drawUsers_v1(coord) {
         //TD:Change
         var iconStyle = new ol.style.Style({
@@ -212,26 +215,42 @@ $(function () {
         });
         vectorSource.addFeature(featurething);
 
+        //POSITION
+        //var points = [];
+
+        //var jsonTest = '[{ "Lat": 41.0, "Lon": 24.1055183 }, { "Lat": 26.0, "Lon": 24.10553 }, { "Lat": 11.0, "Lon": 24.105526 }, { "Lat": -4.0, "Lon": 24.10576 }, { "Lat": -19.0, "Lon": 24.1054878 }, { "Lat": -34.0, "Lon": 24.1058674 }, { "Lat": -49.0, "Lon": 24.1058674 }, { "Lat": -64.0, "Lon": 204.105728 }, { "Lat": -79.0, "Lon": 24.1059246 }, { "Lat": -94.0, "Lon": 24.1054211 }, { "Lat": -109.0, "Lon": 24.1061077 }, { "Lat": -124.0, "Lon": 24.10533 }, { "Lat": -139.0, "Lon": 24.1056042 }, { "Lat": 56.92486, "Lon": 24.1057644 }]';
+        //var jsonArrTest = JSON.parse(jsonTest);
+
+        //for (var i in jsonArrTest) {
+        //    console.log(i);
+        //    console.log(jsonArrTest[i].Lon);
+        //    if (i == 9)
+        //        return;
+        //    var iconFeature1 = new ol.Feature({
+        //        geometry: new ol.geom.Point(ol.proj.transform([jsonArrTest[i].Lon, jsonArrTest[i].Lat], 'EPSG:4326', 'EPSG:3857')),
+        //        name: 'Null Island Two',
+        //        population: 4001,
+        //        rainfall: 501
+        //    });
+        //    iconFeature1.setStyle(styles);
+        //    vectorSource.addFeature(iconFeature1);
+        //}
+
+
         for (var i in coord) {
 
             //createFeature([coord[i].Lon,coord[i].Lat]);
-
-            //var addedMarker = new ol.geom.Point([
-            //    ol.proj.transform([coord[i].Lon, coord[i].Lat], 'EPSG:4326', 'EPSG:3857')]);
-
-
-            //var featurething = new ol.Feature({
-            //    name: "Marker 01",
-            //    geometry: addedMarker
-            //});
-
-            //featurething.setStyle(iconStyle);
-
-            //vectorSource.addFeature(featurething);
-
+            var iconFeature1 = new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.transform([coord[i].Lon, coord[i].Lat], 'EPSG:4326', 'EPSG:3857')),
+                name: 'Null Island Two',
+                population: 4001,
+                rainfall: 501
+            });
+            iconFeature1.setStyle(styles);
+            vectorSource.addFeature(iconFeature1);
+            
         }
-
-
+        
         //usersFeature.setStyle(iconStyle);
         //vectorSource.addFeature(usersFeature);
         //vectorSource.addFeatures(usersFeature);
