92fa2c13043e87fc756e5f245caacdd49407066c
DESKTOP-MRS7EUR\Sevar <emil.nersesjan@btf.lv>
2018-07-05 17:38:53 +0300

    Littl refactor map 2 module

---
+++ MapFinder/Module/Map/MapController.cs
@@ -0,0 +1,34 @@
+﻿using Database;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapFinder.Module.Map
+{
+    public class MapController : Controller
+    {
+        [HttpPost]
+        public JsonResult GetUsers()
+        {
+            using (var db = new ModelDataContext())
+            {
+                return Json(getUsersCoordinteByRange(0));
+            }
+        }
+        private string getUsersCoordinteByRange(float range)
+        {
+            string userJsonCoord;
+
+            using (var db = new ModelDataContext())
+            {
+                var user = db.Users.Select(l => new { l.Lat, l.Lon, l.UserId }).ToList();
+                userJsonCoord = JsonConvert.SerializeObject(user);
+            }
+
+            return userJsonCoord;
+        }
+    }
+}
